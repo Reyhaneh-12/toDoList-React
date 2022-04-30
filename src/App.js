b03c932fd1a97baa09bc2efb1378038054cd5d18
Reyhaneh Savari <reyhaneh.sa.mo@gmail.com>
2022-04-30 21:40:55 +0430

    add "add toDo"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const formHandler = (event) => {
+    event.preventDefault();
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo,
+      complected: false
+    };
+    setTodos([...todos, newTodo]);
+    setTodo("");
+  }
 
   return (
     <div className="App">
@@ -8,7 +23,15 @@ function App() {
         <header className="Header">
           <h1 className="headerTitle">To Do List</h1>
         </header>
+
+        <form onSubmit={formHandler} className="addTodoForm">
+          <h3 className="addTitle">Add To Do:</h3>
+          <input type="text" onChange={ event => { setTodo(event.target.value) }} value={todo} className="addInput" placeholder="add your text to do..." />
+          <button type="submit" className="addBtn">Add To Do</button>
+        </form>
+
         
+
       </div>
     </div>
   );
