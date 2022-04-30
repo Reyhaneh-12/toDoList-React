09a159cab54c009672c64df243ffc7843d5d450a
Reyhaneh Savari <reyhaneh.sa.mo@gmail.com>
2022-05-01 00:09:44 +0430

    add section: to do card

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
 
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
+  const [todoEditing, setTodoEditing] = useState(null);
+  const [textEditing, setTextEditing] = useState("");
+
+  
 
   const formHandler = (event) => {
     event.preventDefault();
@@ -15,6 +19,33 @@ function App() {
     };
     setTodos([...todos, newTodo]);
     setTodo("");
+  }
+
+  const checkHandler = (id) => {
+    const updatedTodo = todos.map(todo => {
+      if(todo.id === id)
+        todo.complected = !todo.complected;
+      return todo;
+      });
+    setTodos(updatedTodo);
+}
+
+  const deleteHandler = (id) => {
+    const updatedTodo = todos.filter(todo => todo.id !== id);
+    setTodos(updatedTodo);
+  }
+
+  const editHandler = (id) => {
+      const updatedTodos = todos.map(todo => {
+        if(todo.id === id){
+          todo.text = textEditing;
+        }
+        return todo;
+      })
+      setTodos(updatedTodos);
+      setTodoEditing(null);
+      setTextEditing("");
+
   }
 
   return (
@@ -30,7 +61,28 @@ function App() {
           <button type="submit" className="addBtn">Add To Do</button>
         </form>
 
-        
+        {
+          todos.map((todo) => 
+            <div key={todo.id} className="todo">
+              <div className="contextTodo">
+                <input type="checkbox" onChange={() => checkHandler(todo.id)} checked={todo.complected} className="ckeckTodo" />
+                {
+                  todoEditing === todo.id ?
+                    <input type="text" onChange={(event) => setTextEditing(event.target.value)} value={textEditing} className="inputTodo" /> :
+                    <div className="textTodo">{todo.text}</div>
+                }
+              </div>
+              <div className="actionTodo">
+                <button onClick={() => deleteHandler(todo.id)} className="actionBtn" >Delete</button>
+                {
+                  todoEditing === todo.id ?
+                  <button onClick={() => editHandler(todo.id)} className="actionBtn" >Submit Edit</button> :
+                  <button onClick={() => setTodoEditing(todo.id)} className="actionBtn" >Editing</button>
+                }
+              </div>
+            </div>
+          )
+        }
 
       </div>
     </div>
